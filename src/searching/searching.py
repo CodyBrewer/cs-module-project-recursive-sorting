df0e5e6d5743ef596da7dda1f375745351534506
Cody Brewer <codybrewerdev@gmail.com>
2020-09-08 17:38:33 -0600

    added comments

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,19 +1,19 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    if len(arr) == 0:
+    if len(arr) == 0: # empty array index not found
         return -1
-    middle_index = (start + end) // 2
-    middle_value = arr[middle_index]
+    middle_index = (start + end) // 2 # midway point
+    middle_value = arr[middle_index] # value of the midway
 
-    if middle_value == target:
-        return middle_index
+    if middle_value == target: # value found 
+        return middle_index # return the index of the found value
     else:
-        if middle_value < target:
-            return binary_search(arr, target, middle_index, end)
-        if middle_value > target:
-            return binary_search(arr, target, start, middle_index)
-
-    return -1
+        if middle_value < target: # if the midpoint value is less than the target value
+            return binary_search(arr, target, middle_index, end) # run binary search starting from the midway point to the end
+        if middle_value > target: # if the midpoint value is greater than the target value
+            return binary_search(arr, target, start, middle_index) # run binary search starting from the start to the midway point
+    
+    return -1 # index not found
 
 
 # STRETCH: implement an order-agnostic binary search
