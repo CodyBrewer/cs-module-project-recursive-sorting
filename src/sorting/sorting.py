e60ed499b40de6dfb300d3938ecfd48fe52bb2d7
Cody Brewer <codybrewerdev@gmail.com>
2020-09-08 17:18:46 -0600

    finished sorting

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,17 +3,59 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
-
-
+    indexA = 0 # current index pointer for arrayA
+    indexB = 0 # current index pointer for arrayB
+    mergeIndex = 0 # index of our merged arr
+          
+    # while indexA is less then the length of arrayA and indexB is less than the arrayB
+    while indexA < len(arrA) and indexB < len(arrB): 
+        # if the value of a is greater than b
+        if arrA[indexA] < arrB[indexB]:
+            # set the current index of the mergeINdex to the current index of A
+            merged_arr[mergeIndex] = arrA[indexA] 
+            # increment the indexA pointer
+            indexA += 1
+        else: 
+            # set the current index of the mergeIndex to the current index of B
+            merged_arr[mergeIndex] = arrB[indexB]
+            # increment the indexB pointer
+            indexB += 1
+        # increment the mergeIndex pointer
+        mergeIndex += 1
+    # while arrayB is not empty but arrayB is
+    while indexA < len(arrA): 
+        # set the current index of the merged_arr to the arrA current index
+        merged_arr[mergeIndex] = arrA[indexA]
+        # increment the indexes
+        indexA += 1
+        mergeIndex += 1
+    # while arrayB
+    while indexB < len(arrB): 
+        # set the current index of the merged_arr to the arrB current index
+        merged_arr[mergeIndex] = arrB[indexB] 
+        # increment the indexes
+        indexB += 1
+        mergeIndex += 1
+    
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # check if arr length is greater than 1
+    if len(arr) > 1:
+        # set the mid variable to be the arr divided by 2
+        middle = len(arr) // 2
+        
+        # set left ot be everything up to the middle of the arr
+        left = arr[:middle]
+        
+        # set right to be everything from the middle to the end
+        right = arr[middle:]
 
-
+        return merge(merge_sort(left), merge_sort(right))
+    print(arr)
     return arr
+
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +63,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
